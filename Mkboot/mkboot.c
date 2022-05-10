4190fec69746ed3ccef2cd164cd28abfa46ae72c
Tom Trebisky <tom@mmto.org>
2022-05-09 17:23:22 -0700

    Hack mkboot to compile under linux

--- Mkboot/mkboot.c
+++ Mkboot/mkboot.c
@@ -10,6 +10,14 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <unistd.h>
+#include <stdlib.h>
+
+/* We are running on a little endian intel machine,
+ * wo we need to byte swap.
+ */
+#define SWAP
+
 #define NSPT	10	/* Rx50 sectors per tracks */
 #define NHEADS	1	/* Rx50 number of heads (sides) */
 #define PCYL	5	/* cylinder where "partition" starts */
@@ -22,56 +30,33 @@
 #endif
 
 #include <fcntl.h>
-#define OFLAGS (O_RDONLY|O_BINARY)
-typedef unsigned long ulong;
+// #define OFLAGS (O_RDONLY|O_BINARY)
+#define OFLAGS (O_RDONLY)
+
+/* On a modern linux machine, int is 4 bytes and long is 8 bytes.
+ * we want nothing to do with 8 byte longs in this software.
+ */
+// typedef unsigned long ulong;
+
+typedef int int4;
+typedef unsigned int u_int4;
+
 struct exec {
-	long	a_magic;	/* number identifies as .o file and gives type of such. */
-	ulong   text;		/* length of text, in bytes */
-	ulong   data;		/* length of data, in bytes */
-	ulong   bss;		/* length of uninitialized data area for file, in bytes */
-	ulong   syms;		/* length of symbol table data in file, in bytes */
-	ulong   entry;		/* start address */
-	ulong   trsize;		/* length of relocation info for text, in bytes */
-	ulong   drsize;		/* length of relocation info for data, in bytes */
+	int4	a_magic;	/* number identifies as .o file and gives type of such. */
+	u_int4   text;		/* length of text, in bytes */
+	u_int4   data;		/* length of data, in bytes */
+	u_int4   bss;		/* length of uninitialized data area for file, in bytes */
+	u_int4   syms;		/* length of symbol table data in file, in bytes */
+	u_int4   entry;		/* start address */
+	u_int4   trsize;		/* length of relocation info for text, in bytes */
+	u_int4   drsize;		/* length of relocation info for data, in bytes */
 } header;
 #define HDRSIZE	sizeof(struct exec)
 
-#ifdef NOT_MSDOS
-/* Back in the old days, on the sun, the a.out.h file was
- * appropriate, but not on a modern linux system.
- */
-#include <a.out.h>
-struct bhdr header;
-#define HDRSIZE	sizeof(struct bhdr)	/* Callan edition 7 unix */
-/* If you can believe it, the Callan byte swaps every sector it writes
- * to the floppy disk - fine for the Callan, but a mess for us, the following
- * switch invokes byte-swapping logic as we write on that machine.
- */
-#define OFLAGS 0
-#define CALLAN
 int ofd;	/* output disk file */
-#endif
-
-#ifdef MSDOS
-#include <dos.h>
-#include <fcntl.h>
-#define OFLAGS (O_RDONLY|O_BINARY)
-typedef unsigned long ulong;
-struct exec {
-	long	a_magic;	/* number identifies as .o file and gives type of such. */
-	ulong   text;		/* length of text, in bytes */
-	ulong   data;		/* length of data, in bytes */
-	ulong   bss;		/* length of uninitialized data area for file, in bytes */
-	ulong   syms;		/* length of symbol table data in file, in bytes */
-	ulong   entry;		/* start address */
-	ulong   trsize;		/* length of relocation info for text, in bytes */
-	ulong   drsize;		/* length of relocation info for data, in bytes */
-} header;
-#define HDRSIZE	sizeof(struct exec)
-#endif
 
 struct ctsup {
-	long	magic;		/* word 1-2 of 512 */
+	int4	magic;		/* word 1-2 of 512 */
 	short	fill0[6];	/* words 3-8 */
 	short	nheads;		/* word 9 0x3010 --> a0@(0xa) */
 	short	sectrk;		/* word 10 0x3012 */
@@ -79,15 +64,17 @@ struct ctsup {
 	char	flags;		/* word 12 0x3016 */
 	char	hdseek;
 	short	fill1[33];	/* word 13-45 0x3018 never used */
-	long	ldrptr;		/* word 46-49 */
+	int4	ldrptr;		/* word 46-49 */
 	short	ldrcnt;	
-	long	hdbptr;		/* word 52-55 */
+	int4	hdbptr;		/* word 52-55 */
 	short	hdbcnt;
-	long	dmpptr;		/* word 58-61 */
+	int4	dmpptr;		/* word 58-61 */
 	short	dmpcnt;
 	short	fill2[456];	/* word 64-511 */
-	long	cksum;		/* word 511-512 */
-} sblock;
+	int4	cksum;		/* word 511-512 */
+} __attribute__((packed));
+
+struct ctsup sblock;
 
 /* The i/o for the boot image is done in 1K granules,
  * in particular, note that the size of the super-block (boot-block)
@@ -97,16 +84,114 @@ struct ctsup {
 #define KSECS 2		/* sectors per K unit */
 #define KSIZE (KSECS*SSIZE)	/* 1K unit */
 
+/* Byte swap a short */
+short 
+sfix( int inword )
+{
+#ifdef SWAP
+	int tmp;
+
+	tmp = inword;
+	inword = (tmp&0xff)<<8 | (tmp>>8)&0xff;
+#endif
+	return(inword);
+}
+
+int4 
+lfix( int4 inlong )
+{
+#ifdef SWAP
+	union {
+		int4	tlong;
+		char	clong[4];	/* the ultimate in portability */
+	} tu;
+	char tmp;
+
+	tu.tlong = inlong;
+	tmp = tu.clong[0];
+	tu.clong[0] = tu.clong[3];
+	tu.clong[3] = tmp;
+	tmp = tu.clong[1];
+	tu.clong[1] = tu.clong[2];
+	tu.clong[2] = tmp;
+	return(tu.tlong);
+#else
+	return(inlong);
+#endif
+}
+
+#ifdef notdef
+int4
+lsum ( int4 *buf, int nlong)
+{
+	unsigned int sum = 0;
+
+	while ( nlong-- )
+		sum += lfix(*buf++);
+	return ( sum );
+}
+#endif
+
+int4
+lsum ( char *buf, int nlong)
+{
+	unsigned int sum = 0;
+	int4 *ip;
+
+	ip = (int4 *) buf;
+	while ( nlong-- )
+		sum += lfix(*ip++);
+	return ( sum );
+}
+
+void
+zfill ( char *buf, int nby)
+{
+	while ( nby-- )
+		*buf++ = 0;
+}
+
+void
+bswap ( char *buf, int n )
+{
+	int t;
+
+	for ( ; n ; n -= 2 ) {
+		t = *buf;
+		*buf = buf[1];
+		buf[1] = t;
+		buf += 2;
+	}
+}
+
+void
+error ( char *s )
+{
+	fprintf(stderr,"%s\n",s);
+	exit(1);
+}
+
+/* bsize must be an integral number of sectors, at present
+ * bsize is always 1024 bytes (KSIZE) in this program.
+ */
+int
+dwrite ( char *buf )
+{
+	bswap(buf,KSIZE);
+	if ( write ( ofd, buf, KSIZE ) != KSIZE )
+		error("Write error");
+}
+
+/* --------------------------------------------------------------------------------- */
+/* --------------------------------------------------------------------------------- */
+/* --------------------------------------------------------------------------------- */
+
 char imbuf[KSIZE];
 
-long lfix();
-short sfix();
-long lsum();
-
-main(argc,argv)
-char **argv;
+int
+main ( int argc, char ** argv )
 {
-	long imsize;
+	int4 imsize;
 	int imblocks;
 /*	struct stat sbuf;	*/
 	int infd;
@@ -118,27 +203,31 @@ char **argv;
 
 	--argc;
 	++argv;
-#ifdef MSDOS
-	if ( argc < 1 )
-		error("usage: mkboot aoutfile");
-#else
+
+	// printf ( "int = %d\n", sizeof(int) );
+	// printf ( "long = %d\n", sizeof(long) );
+
 	if ( argc != 2 )
 		error("usage: mkboot aoutfile bootfile");
-#endif
 
-	if ( sizeof(struct ctsup) != KSIZE )
-		error("Superblock structure malformed\n");
+	if ( sizeof(struct ctsup) != KSIZE ) {
+	    fprintf ( stderr, "Superblock = %d bytes\n", sizeof(struct ctsup) );
+	    error("Superblock structure malformed\n");
+	}
+
 	if ( (infd = open(argv[0],OFLAGS)) == -1 )
 		error("Cannot open input (a.out) file");
 
 	if ( read(infd,&header,HDRSIZE) != HDRSIZE )
 		error("Cannot read a.out header");
+
 /*	if ( fstat(infd,&sbuf) == -1 )
  *		error ("Cannot access input file");
  *
  *	imsize = sbuf.st_size - HDRSIZE;
  *	printf("%ld bytes in image\n",imsize);
  */
+
 	printf("text size: %ld\n",lfix(header.text));
 	printf("data size: %ld\n",lfix(header.data));
 	printf("text + data = %ld\n",lfix(header.text)+lfix(header.data));
@@ -152,7 +241,7 @@ char **argv;
 	
 	zfill ( (char *) &sblock, KSIZE );
 
-	sblock.magic = lfix((long)0x55515651);	/* 'UQVQ' */
+	sblock.magic = lfix((int4)0x55515651);	/* 'UQVQ' */
 #ifdef MSDOS
 	/* proper parameters for a Rainbow RX50 floppy */
 	sblock.nheads = sfix(NHEADS);
@@ -164,19 +253,17 @@ char **argv;
 	sblock.seccyl = sfix(16);	/* sectors per cylinder */
 #endif
 	sblock.flags = 1;		/* double density floppy */
-	sblock.ldrptr = lfix( (long) 1);
+	sblock.ldrptr = lfix( (int4) 1);
 	sblock.ldrcnt = sfix(imblocks);
-	sblock.cksum = lfix ( (long) (-1) -
-			lsum ( (long *) &sblock, (KSIZE/sizeof(long))-1 ));
+	sblock.cksum = lfix ( (int4) (-1) -
+			lsum ( (char *) &sblock, (KSIZE/sizeof(int4))-1 ));
 
-#ifndef MSDOS
+	/* Create output file */
 	if ( (ofd = creat(argv[1],0644)) == -1 )
 		error("Cannot open output file");
-#endif
 
-	dseek ( 0 );
 	dwrite( (char *) &sblock );
-	if ( lseek ( infd, (long) HDRSIZE, 0 ) == -1 )
+	if ( lseek ( infd, (int4) HDRSIZE, 0 ) == -1 )
 		error ("Seek error");
 
 #ifdef DEBUG
@@ -207,189 +294,9 @@ char **argv;
 	close(infd);
 #endif
 
-#ifdef MSDOS
-	dseek ( PCYL * NSPT );
-	if ( argc > 1 ) {
-		if ( (infd = open(argv[1],OFLAGS)) == -1 )
-			error("Cannot open input (fs) file");
-		for ( i=0; ; ) {
-			if ( num = read(infd,imbuf,KSIZE) ) {
-				dwrite ( imbuf );
-				++i;
-			} else
-				break;	/* EOF */
-			if ( num != KSIZE ) {
-				printf("last fs read returned %d\n",num);
-				break;
-			}
-		}
-		printf("%d fs blocks copied\n",i);
-		close(infd);
-	}
-#else
 	close(ofd);
-#endif
+
 	exit(0);
 }
 
-zfill(buf,nby)
-register char *buf;
-register nby;
-{
-	while ( nby-- )
-		*buf++ = 0;
-}
-
-long
-lsum (buf, nlong)
-register long *buf;
-register nlong;
-{
-	unsigned long sum = 0;
-
-	while ( nlong-- )
-		sum += lfix(*buf++);
-	return ( sum );
-}
-
-short 
-sfix(inword)
-{
-#ifdef MSDOS
-	register tmp;
-
-	tmp = inword;
-	inword = (tmp&0xff)<<8 | (tmp>>8)&0xff;
-#endif
-	return(inword);
-}
-
-long 
-lfix(inlong)
-long inlong;
-{
-#ifdef MSDOS
-	union {
-		long	tlong;
-		char	clong[4];	/* the ultimate in portability */
-	} tu;
-	register char tmp;
-
-	tu.tlong = inlong;
-	tmp = tu.clong[0];
-	tu.clong[0] = tu.clong[3];
-	tu.clong[3] = tmp;
-	tmp = tu.clong[1];
-	tu.clong[1] = tu.clong[2];
-	tu.clong[2] = tmp;
-	return(tu.tlong);
-#else
-	return(inlong);
-#endif
-
-}
-
-error(s)
-char *s;
-{
-	fprintf(stderr,"%s\n",s);
-	exit(1);
-}
-
-bswap(buf,n)
-register char *buf;
-register n;
-{
-	register t;
-
-	for ( ; n ; n -= 2 ) {
-		t = *buf;
-		*buf = buf[1];
-		buf[1] = t;
-		buf += 2;
-	}
-}
-
-/* bsize must be an integral number of sectors, at present
- * bsize is always 1024 bytes (KSIZE) in this program.
- */
-#ifdef CALLAN
-dwrite(buf)
-char *buf;
-{
-	bswap(buf,KSIZE);
-	if ( write ( ofd, buf, KSIZE ) != KSIZE )
-		error("Write error");
-}
-#else
-/* this code is specific to the Rainbow.
- * (it would be generic MSDOS except for the Rainbow interleave stuff.)
- */
-static short nextsec = 0;
-
-dseek(newsec)
-{
-	nextsec = newsec;
-}
-
-#define A_DISK	0	/* A: */
-#define B_DISK	1	/* B: */
-#define D_DISK	3	/* D: */
-
-#define DRIVE	D_DISK	/* proper for use with RX50DRVR on the AT */
-
-#define AD_WRITE 0x26	/* Absolute Disk Write is INT 26H */
-
-static char secmap[] = { 0, 5, 1, 6, 2, 7, 3, 8, 4, 9 };
-
-dwrite(buf)
-char *buf;
-{
-	int track;
-
-	track = nextsec / NSPT;
-	if ( track < 2 )	/* first two tracks are 1:1 interleave */
-		swrite(buf,nextsec);
-	else
-		swrite(buf,track*NSPT + secmap[nextsec%NSPT]);
-
-	track = ++nextsec / NSPT;
-	if ( track < 2 )
-		swrite(&buf[SSIZE],nextsec);
-	else
-		swrite(&buf[SSIZE],track*NSPT + secmap[nextsec%NSPT]);
-	++nextsec;
-}
-
-swrite(buf, secabs)
-char *buf;
-{
-	union REGS inregs, outregs;
-
-	inregs.h.al = DRIVE;	/* write to A: */
-	inregs.x.cx = 1;	/* write one 512 byte unit */
-	inregs.x.dx = secabs;
-/*
- * This would be fine if buf was a (far char *).
- *	inregs.x.bx = FP_OFF(buf);
- */
-	inregs.x.bx = (unsigned int) buf;	/* Yuk! */
-
-	printf("Writing sector %d (to %d)\n",nextsec,secabs);
-
-	int86 ( AD_WRITE, &inregs, &outregs );
-	/* one uncertain aspect of this call is that it leaves the flags
-	   on the stack when it returns (why, WHY? I ask you), there is no
-	   clean way to tidy this up in C, but I expect that the subroutine
-	   epilog will get things straightened up.
-	*/
-
-	if ( outregs.x.cflag ) {
-		printf("error writing sector %d\n",nextsec);
-		printf("cflag = %04x\n",outregs.x.cflag);
-		printf("   ax = %04x\n",outregs.x.ax);
-		exit(1);
-	}
-}
-#endif
 /* END */
